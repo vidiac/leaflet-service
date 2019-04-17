082302e58c338bb738d28e22547c1b3f96d96ddd
Vidineev Sergey <vidiac@yandex.ru>
2019-04-17 14:48:05 +1000

    update webpack build

---
+++ webpack.config.js
@@ -0,0 +1,18 @@
+var path = require("path");
+var webpack = require("webpack");
+
+module.exports = {
+  entry: "./src/index.js",
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: "index.js"
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        loader: "babel-loader",
+      }
+    ]
+  },
+};
