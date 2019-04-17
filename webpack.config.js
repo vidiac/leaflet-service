3a361ade9e6ece7355821aa4ebbc714194c4e5c0
Vidineev Sergey <vidiac@yandex.ru>
2019-04-17 14:59:27 +1000

    update export

--- webpack.config.js
+++ webpack.config.js
@@ -5,7 +5,10 @@ module.exports = {
   entry: "./src/index.js",
   output: {
     path: path.resolve(__dirname, 'dist'),
-    filename: "index.js"
+    filename: "index.js",
+    library: "index.js",
+    libraryTarget: 'umd',
+    umdNamedDefine: true
   },
   module: {
     rules: [
